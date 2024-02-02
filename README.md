22e47203e4c2d93ef1d15bc622665da5b7d4f465
getinthere <getinthere@naver.com>
2024-02-02 17:14:06 +0900

    view 세팅 완료

---
+++ README.md
@@ -0,0 +1,24 @@
+# 스프링부트 블로그 만들기
+
+## 기술스택
+
+- Springboot 2.7
+- JDK 11
+- VSCODE
+- MySQL8.0
+
+## 의존성
+
+- Lombok
+- DevTools
+- Spring WEB
+- JPA
+- h2
+- MySQL
+- Mustache
+
+## 프로젝트 시작
+
+```sql
+create database blogdb;
+```
---
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping({ "/", "/board" })
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/1")
+    public String detail() {
+        return "board/detail";
+    }
+}
