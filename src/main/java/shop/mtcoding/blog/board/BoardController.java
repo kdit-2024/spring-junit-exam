3238af79ee6e7b9f0829e799f87861b973c10004
getinthere <getinthere@naver.com>
2024-02-07 16:43:08 +0900

    디지ì자인 완료

---
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -0,0 +1,41 @@
+package shop.mtcoding.blog.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable int id) {
+        return "board/updateForm";
+    }
+
+    @PostMapping("/board/save")
+    public String save(){
+        return "redirect:/";
+    }
+
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable int id){
+        return "redirect:/";
+    }
+
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable int id){
+        return "redirect:/";
+    }
+}
